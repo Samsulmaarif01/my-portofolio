72bf5305e99c65ad93ca46029a5aa5f45cf1b56d
Samsul Maarif <samsulmaarif1076@gmail.com>
2024-10-06 11:43:56 +0700

    chacge layout text on mobile view

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          className="col-span-8 place-self-center text-center sm:text-left sm:justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-secondarytext-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500 block-600">
